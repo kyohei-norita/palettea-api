2cbf14b9f37fe0ddb80b5b3f98b6b70c2e0379f4
norita <kyohei.norita@gmail.com>
2024-04-10 22:46:04 +0900

    first commit

---
+++ src/main/java/palettea/api/PaletteaApiApplication.java
@@ -0,0 +1,13 @@
+package palettea.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaletteaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaletteaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/palettea/api/PaletteaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package palettea.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaletteaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
